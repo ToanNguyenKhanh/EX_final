237439078a6a6e7f8ab9aa9dfdf77ad2d6ae35e5
TKN <111730598+ToanNguyenKhanh@users.noreply.github.com>
2022-10-30 23:25:52 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+/*Bài 1: 
+Nhập vào 3 số, kiểm tra chúng có tạo thành 3 cạnh của một tam giác không? Tính ba góc của tam giác đó (nếu thỏa mãn)
+Bài 2:
+P = 1/√(n+1&1+√(n+1&2+√(n+1&3+⋯+√(n+1&n+1))))
+Có n+1 dấu căn. Với n = 5
+Bài 3: 
+Dùng mảng 1 chiều để in ra n số hạng đầu tiên của dãy số Fibonacci
+Bài 4: 
+Nhập vào 2 ma trận có cùng cấp Amxn và Bmxn. In ra kết quả của ma trận            Cmxn =Amxn + Bmxn.
+Bài 5: 
+Đọc vào một chuỗi, đổi sang chuỗi chữ hoa đầu mỗi từ. Ví dụ S= “Cong nghe thong tin KINH doaNH”. Kết quả in ra “Cong Nghe Thong Tin Kinh Doanh”
+Bài 6: 
+Cho mảng nhiều chiều gồm có các thông tin liên quan đến khách lưu trú ở khách sạn. Khách gồm các thông tin: họ tên, số ngày ở, số bữa an, tiền ở (1 ngày), tiền ăn (1 bữa), phí phục vụ. Phân tích và viết chương trình tính tiền trọ khách sạn. In hóa đơn tính tiền lên màn hình.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
